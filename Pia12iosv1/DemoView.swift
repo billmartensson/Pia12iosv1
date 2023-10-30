0000a0b8bf1eb156a4f7fa36b71869217eb6810b
Bill Mårtensson <bill@magictechnology.se>
2023-10-30 13:29:23 +0100

    Pia12

---
+++ Pia12iosv1/DemoView.swift
@@ -0,0 +1,18 @@
+//
+//  DemoView.swift
+//  Pia12iosv1
+//
+//  Created by BillU on 2023-10-30.
+//
+
+import SwiftUI
+
+struct DemoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DemoView()
+}
